cb88b68b76c27632af3901510e9daceaecc425ac
JosyanJr <josyan_jr_@hotmail.com>
2024-05-30 14:08:45 -0300

    Update importProdutos.py

--- importProdutos.py
+++ importProdutos.py
@@ -20,7 +20,7 @@ def gerar_sql_inserts(df):
     for index, row in df.iterrows():
         id = row['proid']
         Cst2 = str(row['proCodCst2']).zfill(2)
-        insert = f"insert into produto_empresa(empid, preid, proid, proCodigo, ProCusto, ProVenda, ProUn, ProUnTrib, proUnComercialId, proUnTribId, {Cst2}, proCodCSOSN, proListaInvent, proEstoqueMin) VALUES(1, {id}, {id}, {row['proCodigo']}, {row['ProCusto']}, {row['ProVenda']}, '{row['ProUn']}', '{row['ProUnTrib']}', {row['proUnComercialId']}, {row['proUnTribId']}, '{row['proCodCst2']}', {row['proCodCSOSN']}, '-1', 1) go"
+        insert = f"insert into produto_empresa(empid, preid, proid, proCodigo, ProCusto, ProVenda, ProUn, ProUnTrib, proUnComercialId, proUnTribId, proCodCst2, proCodCSOSN, proListaInvent, proEstoqueMin) VALUES(1, {id}, {id}, {row['proCodigo']}, {row['ProCusto']}, {row['ProVenda']}, '{row['ProUn']}', '{row['ProUnTrib']}', {row['proUnComercialId']}, {row['proUnTribId']}, '{Cst2}', '{row['proCodCSOSN']}', '-1', 1) go"
         inserts.append(insert)
     inserts.append('SET IDENTITY_INSERT produto_empresa OFF GO')
 
@@ -37,8 +37,10 @@ def salvar_sql_inserts(arquivo, inserts):
             f.write(insert + '\n')
 
 if __name__ == "__main__":
+
+  
     # Carregar os dados do Excel
-    df = pd.read_excel('dadosProdutos.xlsx', sheet_name='Plan1')
+    df = pd.read_excel("C:\\Users\\Elson Jr\\Downloads\\Import-Produto-main\\dadosProdutos.xlsx", sheet_name='Plan1')
 
     # Substituir campos vazio por 'Null'
     df = df.fillna('Null')
@@ -47,6 +49,6 @@ if __name__ == "__main__":
     inserts = gerar_sql_inserts(df)
 
     # Salvar os inserts em um arquivo
-    arquivo = 'insertsProdutos.txt'
+    arquivo = "C:\\Users\\Elson Jr\\Downloads\\Import-Produto-main\\insertsProdutos.txt"
     salvar_sql_inserts(arquivo, inserts)
     print(f"Arquivo {arquivo} gerado com sucesso.")
